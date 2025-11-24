76f06ba61bc085d6996cf4c6960782e5694862cf
Daniel <danieljamespierce@gmail.com>
2025-11-24 21:30:24 +0000

    Started an index page

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
 
 const PORT = 3000;
 
 const app = express();
 
+// Set up Handlebars
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
+
 app.use((req, res) => {
-    res.send("Hello World");
+    res.render('index', {});
 });
 
-app.listen(PORT, () => {});
+app.listen(PORT, () => {
+    console.log(`Listening on Port ${PORT}`);
+});
