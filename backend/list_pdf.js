7c5c7bbce3fed02e86fdfa765b5cc25f2fac55b5
Daniel <danieljamespierce@gmail.com>
2025-11-25 14:59:04 +0000

    Added some comments

--- backend/list_pdf.js
+++ backend/list_pdf.js
@@ -5,11 +5,14 @@ pdf_location = path.join(__dirname, 'pdfs');
 meta_location = path.join(__dirname, 'meta');
 
 function findPdfs() {
+    // Read the directory of pdfs
     files = fs.readdirSync(pdf_location);
     file_data = [];
     files.forEach(file => {
+        // Get metdata
         json = fs.readFileSync(path.join(meta_location, file)+'.json');
         data = JSON.parse(json);
+        // Add the file location with the metadata
         data.file = file;
         file_data.push(data);
     });
